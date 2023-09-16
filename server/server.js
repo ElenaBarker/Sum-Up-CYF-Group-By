0808955bba982cecc97ee5feb116419fa6f4a674
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-09-16 12:32:49 +0100

    backend first commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5500;
 
 app.get("/", (req, res) => {
-    res.send("Sum Up CYF Group By")
-})
+  res.send("Sum Up CYF Group By");
+});
 
-
+app.post("/", (req, res) => {
+  res.send("");
+});
 
 app.listen(port, () => {
-    console.log("Sum Up CYF Group By listening on port 5005..")
-})
+  console.log("Sum Up CYF Group By listening on port 5500..");
+});
